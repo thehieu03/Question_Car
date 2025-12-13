ead2c2da71ca715ae42ac4c1d67ef109199969cd
ThuLe <vinhantran06@Gmail.com>
2025-12-13 22:07:41 +0700

    udpate done admin

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Person.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Dell
+ */
+public class Person {
+//    int 
+//    double 
+//    float
+        int 
+                double 
+                        float
+}
